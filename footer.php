9f5a8017115c46f0c03c6f9bebc0c361f15bdaf7
rholibobo <rholipop@gmail.com>
2023-02-02 00:41:52 +0100

    CBHI

---
+++ footer.php
@@ -0,0 +1,162 @@
+<footer class="site-footer">
+            <img src="assets/images/icons/footer-bg-icon-1.png" class="site-footer__shape-1" alt="">
+            <img src="assets/images/icons/footer-bg-icon-2.png" class="site-footer__shape-2" alt="">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                        <div class="footer-widget">
+                            <a href="index.html" class="footer-widget__Logo">
+                                <img src="assets/images/logo-light.png" width="153" alt="">
+                            </a>
+                            <p>CapacityBay Health Initiative (CBHI) is a not-for-profit healthcare and humanitarian relief agency
+                                which focuses on and specializes in medical and emergency relief services in rural communities
+                            </p>
+                            <form action="#" data-url="YOUR_MAILCHIMP_URL" class="mc-form">
+                                <input type="email" name="EMAIL" placeholder="Email Address">
+                                <button type="submit"><i class="agrikon-icon-right-arrow"></i></button>
+                            </form><!-- /.mc-form -->
+                            <div class="mc-form__response"></div><!-- /.mc-form__response -->
+                            <div class="footer__social">
+                                <a href="#" class="fab fa-facebook-square"></a>
+                                <a href="#" class="fab fa-twitter"></a>
+                                <!-- <a href="#" class="fab fa-pinterest-p"></a> -->
+                                <a href="#" class="fab fa-instagram"></a>
+                            </div><!-- /.topbar__social -->
+                        </div><!-- /.footer-widget -->
+                    </div><!-- /.col-sm-12 col-md-6 col-lg-4 -->
+                    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-2">
+                        <div class="footer-widget footer-widget__links-widget">
+                            <h3 class="footer-widget__title">Links</h3><!-- /.footer-widget__title -->
+                            <ul class="list-unstyled footer-widget__links">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about-us.html">About us</a></li>
+                                <li><a href="what-we-do.html">What We Do</a></li>
+                                <li><a href="getinvolved.html">Get Involved</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                                <li><a href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">CoronaVirus Update</a></li>
+                            </ul><!-- /.list-unstyled -->
+                        </div><!-- /.footer-widget -->
+                    </div><!-- /.col-sm-12 col-md-6 col-lg-2 -->
+                    
+                    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                        <h3 class="footer-widget__title">Contact</h3><!-- /.footer-widget__title -->
+                        <ul class="list-unstyled footer-widget__contact">
+                            <li>
+                                <i class="agrikon-icon-telephone"></i>
+                                <a href="tel:301-970-9800">301 970 9800</a>
+                            </li>
+                            <li>
+                                <i class="agrikon-icon-email"></i>
+                                <a href="mailto:medicaldirector@capacitybayhealth.org">medicaldirector@capacitybayhealth.org</a>
+                            </li>
+                            <li>
+                                <i class="agrikon-icon-pin"></i>
+                                <a href="#">9701 Apollo Drive Largo, MD 20774 USA</a>
+                            </li>
+                        </ul><!-- /.list-unstyled -->
+                    </div><!-- /.col-sm-12 col-md-6 col-lg-3 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </footer><!-- /.site-footer -->
+        <div class="bottom-footer">
+            <div class="container">
+                <p>Copyright © 2023 | CapacityBay Health Initiative | All rights reserved</p>
+                <div class="bottom-footer__links">
+                    <a href="#">Terms & Conditions</a>
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Sitemap</a>
+                </div><!-- /.bottom-footer__links -->
+            </div><!-- /.container -->
+        </div><!-- /.bottom-footer -->
+
+    </div><!-- /.page-wrapper -->
+
+
+    <div class="mobile-nav__wrapper">
+        <div class="mobile-nav__overlay mobile-nav__toggler"></div>
+        <!-- /.mobile-nav__overlay -->
+        <div class="mobile-nav__content">
+            <span class="mobile-nav__close mobile-nav__toggler"><i class="far fa-times"></i></span>
+
+            <div class="logo-box">
+                <a href="index.html" aria-label="logo image"><img src="assets/images/logo-light.png" width="155"
+                        alt="" /></a>
+            </div>
+            <!-- /.logo-box -->
+            <div class="mobile-nav__container"></div>
+            <!-- /.mobile-nav__container -->
+
+            <ul class="mobile-nav__contact list-unstyled">
+                <li>
+                    <i class="agrikon-icon-email"></i>
+                    <a href="mailto:medicaldirector@capacitybayhealth.org">medicaldirector@capacitybayhealth.org</a>
+                </li>
+                <li>
+                    <i class="agrikon-icon-telephone"></i>
+                    <a href="tel:301-970-9800">301 970 9800</a>
+                </li>
+            </ul><!-- /.mobile-nav__contact -->
+            <div class="mobile-nav__top">
+                <div class="mobile-nav__language">
+                    <img src="assets/images/resources/flag-1-1.jpg" alt="">
+                    <label class="sr-only" for="language-select">select language</label>
+                    <!-- /#language-select.sr-only -->
+                    <select class="selectpicker" id="language-select">
+                        <option value="english">English</option>
+                        <option value="arabic">Arabic</option>
+                    </select>
+                </div><!-- /.mobile-nav__language -->
+                <div class="mobile-nav__social">
+                    <a href="#" aria-label="twitter"><i class="fab fa-twitter"></i></a>
+                    <a href="#" aria-label="facebook"><i class="fab fa-facebook-square"></i></a>
+                    <!-- <a href="#" aria-label="pinterest"><i class="fab fa-pinterest-p"></i></a> -->
+                    <a href="#" aria-label="instagram"><i class="fab fa-instagram"></i></a>
+                </div><!-- /.mobile-nav__social -->
+            </div><!-- /.mobile-nav__top -->
+
+
+
+        </div>
+        <!-- /.mobile-nav__content -->
+    </div>
+    <!-- /.mobile-nav__wrapper -->
+
+    <div class="search-popup">
+        <div class="search-popup__overlay search-toggler"></div>
+        <!-- /.search-popup__overlay -->
+        <div class="search-popup__content">
+            <form action="#">
+                <label for="search" class="sr-only">search here</label><!-- /.sr-only -->
+                <input type="text" id="search" placeholder="Search Here..." />
+                <button type="submit" aria-label="search submit" class="thm-btn">
+                    <i class="fa fa-search"></i>
+                </button>
+            </form>
+        </div>
+        <!-- /.search-popup__content -->
+    </div>
+    <!-- /.search-popup -->
+
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+
+
+    <script src="assets/js/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/swiper.min.js"></script>
+    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/odometer.min.js"></script>
+    <script src="assets/js/jquery.appear.min.js"></script>
+    <script src="assets/js/jarallax.min.js"></script>
+    <script src="assets/js/circle-progress.min.js"></script>
+    <script src="assets/js/wNumb.min.js"></script>
+    <script src="assets/js/nouislider.min.js"></script>
+
+    <!-- template js -->
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home || CapacityBay Health </title>
+    <!-- favicons Icons -->
+
+    <link rel="icon" type="image/png" href="assets/images/favicons/favicon.png">
+
+    <link rel="manifest" href="assets/images/favicons/site.webmanifest">
+    <meta name="description" content="CapacityBay Health Initiatives">
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Handlee&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    <link rel="stylesheet" href="assets/css/jarallax.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="assets/css/agrikon-icons.css">
+    <link rel="stylesheet" href="assets/css/nouislider.min.css">
+    <link rel="stylesheet" href="assets/css/nouislider.pips.css">
+    <link rel="stylesheet" href="assets/css/donation.css">
+
+    <!-- template styles -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <!-- <div class="preloader">
+        <img class="preloader__image" width="55" src="assets/images/loader.png" alt="">
+    </div>/.preloader -->
+    <div class="page-wrapper">
+
+        <header class="main-header">
+            <div class="topbar">
+                <div class="container">
+                    <div class="topbar__left">
+                        <div class="topbar__social">
+                            <a href="#" class="fab fa-facebook-square"></a>
+                            <a href="#" class="fab fa-twitter"></a>
+                            <a href="#" class="fab fa-pinterest-p"></a>
+                            <a href="#" class="fab fa-instagram"></a>
+                        </div><!-- /.topbar__social -->
+                        <p>Welcome to CapacityBay Health Initiative</p>
+                    </div><!-- /.topbar__left -->
+                    <div class="topbar__right">
+                        <a href="#"><i class="agrikon-icon-email"></i>info@capacitybayhealth.org</a>
+                        <a href="#"><i class="agrikon-icon-clock"></i>Mon - Sat 8:00 - 6:30, Sunday - CLOSED</a>
+                    </div><!-- /.topbar__right -->
+                </div><!-- /.container -->
+            </div><!-- /.topbar -->
+            <nav class="main-menu">
+                <div class="container">
+                    <div class="logo-box">
+                        <a href="index.html" aria-label="logo image"><img src="assets/images/logo-dark.png" width="153"
+                                alt=""></a>
+                        <span class="fa fa-bars mobile-nav__toggler"></span>
+                    </div><!-- /.logo-box -->
+                    <ul class="main-menu__list">
+                        <li class="dropdown">
+                            <a href="index.html">Home</a>
+
+                        </li>
+                        <li>
+                            <a href="about-us.html">About Us</a>
+                        </li>
+                        <li class="dropdown"><a href="what-we-do.html">What we do</a>
+                            <ul>
+
+                                <li><a href="action.html">Action to Improve Health Literacy</a></li>
+                                <li><a href="medical-mission.html">Nigeria's Medical Mission & Volunteerism</a></li>
+                                <li><a href="medical-finance.html">Medical Financial Assistance Program</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="getinvolved.html">Get Involved</a>
+                            <ul>
+                                <li><a href="support.html">Support us</a></li>
+                                <li><a href="signup.html">Sign up</a></li>
+
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="contact.html">Contact Us</a>
+
+                        </li>
+
+                        <li class="dropdown"><a href="https://gisanddata.maps.arcgis.com/apps/dashboards/index.html#/bda7594740fd40299423467b48e9ecf6">CoronaVirus</a>
+                            <ul>
+
+                                <li><a href="https://covid19.ncdc.gov.ng/">NCDC Report</a></li>
+                                <li><a href="https://www.cdc.gov/Coronavirus/2019-ncov/index.html">Covid-19 Report</a></li>
+                                <li><a href="https://gisanddata.maps.arcgis.com/apps/dashboards/index.html#/bda7594740fd40299423467b48e9ecf6">Virus Map</a></li>
+                                <li><a href="https://www.whitehouse.gov/covidplan/">Guidelines</a></li>
+                                <li><a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public?utm_source=desktop-snippet&utm_medium=snippet&utm_campaign=Health2020&utm_term=22988&utm_content=REL">WHO</a></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                    <!-- /.main-menu__list -->
+
+                    <div class="main-header__info">
+
+                        
+                    </div><!-- /.main-header__info -->
+                    &nbsp;&nbsp;
+                    <div class="main-header__info">
+
+                        <a href="support.html" class="main-header__info-phone" style="background-color: #255946;">
+                            <!-- <i class="agrikon-icon-phone-call"></i> -->
+                            <span class="main-header__info-phone-content">
+                                <!-- <span class="main-header__info-phone-text">Call Anytime</span> -->
+                                <span class="main-header__info-phone-title">Support Us</span>
+                            </span><!-- /.main-header__info-phone-content -->
+                        </a><!-- /.main-header__info-phone -->
+                    </div><!-- /.main-header__info -->
+                </div><!-- /.container -->
+            </nav>
+            <!-- /.main-menu -->
+        </header><!-- /.main-header -->
+        
